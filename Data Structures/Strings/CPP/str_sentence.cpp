35c18c134b518e84bf1dd8e34f5905a924a8250f
Aheli Ghosh <debjanihome@gmail.com>
2018-10-18 23:11:44 +0530

    changed indentation

--- Data Structures/Strings/CPP/str_sentence.cpp
+++ Data Structures/Strings/CPP/str_sentence.cpp
@@ -2,20 +2,26 @@
 #include <cstring>
 using namespace std;
 
-void rem_word(char str[100], char word[50], int *n){
+void rem_word(char str[100], char word[50], int *n)
+{
 	int len = strlen(word);
 	int flag;
-	for(int i = 0; i < *n; i++){
-		if(!isalpha(str[i + len])){
-		int flag = 1;
-			for(int j = 0; j < len; j++){
-				if(str[i + j] != word[j]){
+	for(int i = 0; i < *n; i++)
+	{
+		if(!isalpha(str[i + len]))
+		{
+			int flag = 1;
+			for(int j = 0; j < len; j++)
+			{
+				if(str[i + j] != word[j])
+				{
 					flag = 0;
 					break;
 				}
 			}
 			if(flag == 1){
-				for(int k = i; k < *n; k++){
+				for(int k = i; k < *n; k++)
+				{
 					str[k] = str[k + len];
 					(*n) = (*n) - len;
 				}
